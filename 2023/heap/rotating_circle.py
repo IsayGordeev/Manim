c5d11ac54fab03f4637b3774cbffcdcfd2f34903
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-02-18 03:37:06 +0100

    18 février 2023

--- 2023/heap/rotating_circle.py
+++ 2023/heap/rotating_circle.py
@@ -2,21 +2,7 @@ from manim import *
 
 class RotatingCircleOnParabola(Scene):
     def construct(self):
-        TAU = 2
-        parabola = ParametricFunction(
-            lambda t: np.array([t, t**2, 0]),
-            t_min=-TAU, t_max=TAU,
-        )
-        parabola_path = ParametricFunction(
-            lambda t: np.array([t, t**2, 0]),
-            t_min=-TAU, t_max=TAU,
-        )
+        text = Tex('awd')
 
-        circle = Circle(radius=0.5)
-        circle.move_to(parabola.point_from_proportion(0))
+        self.play(Create(text))
 
-        moving_circle = MovingAlongPath(circle, parabola_path)
-        self.add(parabola)
-        self.add(moving_circle)
-
-        self.play(Rotating(moving_circle, about_point=parabola.point_from_proportion(0),radians=TAU, run_time=5), moving_circle.update)
